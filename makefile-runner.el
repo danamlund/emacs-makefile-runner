66d6c3132d90d23d6d03b27d0413cb359c9886dd
Dan Amlund Thomsen <dan@danamlund.dk>
2017-07-29 16:03:19 +0200

    v. 1.2.0 Add GPL license

--- makefile-runner.el
+++ makefile-runner.el
@@ -4,10 +4,25 @@
 
 ;; Author: Dan Amlund Thomsen <dan@danamlund.dk>
 ;; URL: http://danamlund.dk/emacs/make-runner.html
-;; Version: 1.1.2
+;; Version: 1.2.0
 ;; Created: 2009-01-01
 ;; By: Dan Amlund Thomsen
 ;; Keywords: makefile, make, ant, build
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -45,6 +60,7 @@
 ;; /* -*- makefile-runner--makefile: "../Makefile" -*- */
 
 ;;; Changelog:
+;; (2017-07-29) 1.2.0: Add GPL license
 ;; (2012-09-29) 1.1.2: Now also searches for makefiles next to the current file
 ;; (2012-09-29) 1.1.1: Better handles no-makefile-found.
 ;; (2012-09-29) 1.1.0: Added ant support. minibuffer now shows
